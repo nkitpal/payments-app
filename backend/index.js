350d624f8ba4046a429511b535b6b9bfad6f41e9
nkitpal <plnkt61@gmail.com>
2024-03-18 01:11:16 +0530

    variable port added

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1", mainRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 })
